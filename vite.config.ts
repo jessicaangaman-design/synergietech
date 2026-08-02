67836b144b91e916d97bc70d60f4cd79050a65f7
Henoc0 <henocbenikoffi@gmail.com>
2026-08-02 13:17:47 +0000

    Connexion des données des interventions avec la BD depuis l'application

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const apiProxyTarget =
-    env.VITE_API_PROXY_TARGET || "https://backless-congenial-freezing.ngrok-free.dev";
+    env.VITE_API_PROXY_TARGET || "http://localhost:3333";
 
   return {
     plugins: [react(), tailwindcss()],
